d735c8ac5489d2eba70c16eb3b793a169d2e413a
Kyando2 <amehikoji@gmail.com>
2020-09-12 14:41:23 +0900

    Made the way settings stored variables look cleaner

--- PyBlox2/Settings.py
+++ PyBlox2/Settings.py
@@ -1,8 +1,13 @@
+def _cool_pop(payload, name):
+    return bool(payload.pop(name))
+
 class BloxSettings:
     
     def __init__(self, payload):
-        self.is_approval_required = bool(payload.pop("isApprovalRequired"))
-        self.is_builders_club_required = bool(payload.pop("isBuildersClubRequired"))
-        self.are_enemies_allowed = bool(payload.pop("areEnemiesAllowed"))
-        self.are_group_games_visible = bool(payload.pop("areGroupGamesVisible"))
-        self.are_group_funds_visible = bool(payload.pop("areGroupFundsVisible"))
+        self.is_approval_required = _cool_pop(payload, "isApprovalRequired")
+        self.is_builders_club_required = _cool_pop(payload, "isBuildersClubRequired")
+        self.are_enemies_allowed = _cool_pop(payload, "areEnemiesAllowed")
+        self.are_group_games_visible = _cool_pop(payload, "areGroupGamesVisible")
+        self.are_group_funds_visible = _cool_pop(payload, "areGroupFundsVisible")
+
+
