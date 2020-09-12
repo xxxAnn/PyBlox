46af12a56b5f18efc89d22b3b4c7ea002f3a9ad7
Kyando2 <amehikoji@gmail.com>
2020-09-12 14:04:18 +0900

    Hid the __fetchable list from BloxTypes

--- PyBlox2/Base.py
+++ PyBlox2/Base.py
@@ -6,25 +6,25 @@ class BloxType():
     '''
     def __init__(self, client):
         self.client = client
-        self._fetchable = []
+        self.__fetchable = []
 
     async def fetch(self, attr: str):
-        if attr in self._fetchable:
+        if attr in self.__fetchable:
             resp = await self._fetcher(attr)
             setattr(self, "_"+attr, resp)
             return resp
 
     def can_fetch(self, *data):
-        self._fetchable.extend(data)
+        self.__fetchable.extend(data)
 
-    async def _fetcher(self, attr): # Default implementation of _fetcher
+    async def _fetcher(self, attr):
         coro = getattr(self, "fetch_"+attr) 
         if coro == None:
             raise NotImplementedError("Attribute {0} hasn't been implemented yet for {1}".format(attr, self.__class__.__name__))
         return await coro()
     
     def __getattr__(self, attr):
-        if attr in self._fetchable:
+        if attr in self.__fetchable:
             try:
                 return getattr(self, "_"+attr)
             except AttributeError:
