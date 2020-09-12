d97df018cb7ca0c70bf3507e997b0dbba8305d7d
Kyando2 <amehikoji@gmail.com>
2020-09-12 14:18:44 +0900

    Removed useless documentation in BloxMember

--- PyBlox2/Member.py
+++ PyBlox2/Member.py
@@ -4,27 +4,11 @@ from .utils import Url
 
 
 class BloxMember(BloxUser):
-    '''
-    A member of a group in Roblox
 
-    Is a descendant of PyBlox2.BloxUser
-
-    ---------------
-
-    Unique methods:
-        coroutine set_role(role) -> role must be a PyBlox2.BloxRank
-        coroutine kick()
-
-    ---------------
-
-    Unique attributes:
-        group -> a PyBlox2.BloxGroup
-
-    '''
     def __init__(self, client, user_id: str, username: str, group):
         super().__init__(client=client, user_id=user_id, username=username)
         self.group = group
-        self.__access = Url("groups", "/v1/groups/%group_id%/users/%user_id%", group_id=self.group.id, user_id=self.id)
+        self.__access = Url("groups", "/v1/groups/%group_id%/users/%user_id%", group_id=self.group.id, user_id=self.id) 
 
     async def set_role(self, role: BloxRank):
         '''
