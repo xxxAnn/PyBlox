5b846a64954a9e386443d8e22f9d9b696947897c
Kyando2 <amehikoji@gmail.com>
2020-09-30 10:48:28 -0400

    Removed already implicit documentation

--- Docs/modules.rst
+++
@@ -1,7 +0,0 @@
-PyBlox2
-=======
-
-.. toctree::
-   :maxdepth: 4
-
-   PyBlox2
--- PyBlox2/utils/Http.py
+++ PyBlox2/utils/Http.py
@@ -1,38 +1,3 @@
-"""
-The most important module and the core of the library, the `Http` utility
-
-Contents:
-    `HttpClient`: No parents
-    `Url`: No parents
-
-Requires:
-    N/A
-
-The following code is provided with: 
-
-    The MIT License (MIT)
-
-    Copyright (c) Kyando 2020
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-"""
-
 import logging
 import re
 import asyncio
@@ -206,7 +171,7 @@ class Url:
         return Url("", url, **fullparams)
 
     # HTTP methods 
-    
+
     async def get(self, data=None, headers=None):
         return await self.__http.request("GET", self.__url, data, headers)
 
