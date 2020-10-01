9d10e8bb67d746bcca58fdee8117f9d1767b82e8
キャンド <felixmd2006@gmail.com>
2020-10-01 16:43:17 -0400

    Revert "Async"

--- Docs/conf.py
+++ Docs/conf.py
@@ -63,7 +63,7 @@ language = 'en'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 # The full version, including alpha/beta/rc tags.
-release = '1.0.1'
+release = '1.0.0'
 
 
 # -- Options for HTML output ----------------------------------------------
